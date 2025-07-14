43b20ac7eb9d6c20e034f07c5f12f4db35340587
AllOne-prog <yuliantikadir2@gmail.com>
2025-07-14 16:20:35 +0800

    multiply_divide

---
+++ multiply_divide.py
@@ -0,0 +1,8 @@
+class MultiplyDivide:
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Pembagi tidak boleh nol!")
+        return a / b
